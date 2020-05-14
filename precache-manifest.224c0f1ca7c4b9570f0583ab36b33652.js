64ed2796b82dbe152ee6565fe33c050ece9dc2c1
Craig Bojko <craig@pixelventures.co.uk>
2020-05-14 12:21:05 +0100

    Updates

---
+++ precache-manifest.224c0f1ca7c4b9570f0583ab36b33652.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e7e8db0d53ab9a908267f3a9cf1f4d",
+    "url": "/Brewdog-Mobile-App-Demo/index.html"
+  },
+  {
+    "revision": "b91308282d9ba954d043",
+    "url": "/Brewdog-Mobile-App-Demo/static/css/2.d1d58843.chunk.css"
+  },
+  {
+    "revision": "aaab162cd542c09bd3f2",
+    "url": "/Brewdog-Mobile-App-Demo/static/css/main.30a8ccbc.chunk.css"
+  },
+  {
+    "revision": "b91308282d9ba954d043",
+    "url": "/Brewdog-Mobile-App-Demo/static/js/2.8e271a88.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/Brewdog-Mobile-App-Demo/static/js/2.8e271a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaab162cd542c09bd3f2",
+    "url": "/Brewdog-Mobile-App-Demo/static/js/main.7b51d275.chunk.js"
+  },
+  {
+    "revision": "4686dbdb8899cc1ad16e",
+    "url": "/Brewdog-Mobile-App-Demo/static/js/runtime-main.89eb2d96.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Brewdog-Mobile-App-Demo/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
